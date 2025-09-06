acbb1723749883a6662f309c0e97b9bb72ade2f2
Mirko <18686886+Mirko-Wittwer@users.noreply.github.com>
2025-09-06 16:10:30 +0200

    Add files via upload

---
+++ Dictionaries_Kontakt.py
@@ -0,0 +1,12 @@
+# 1. Erstelle das Dictionary
+kontakt = {
+    "name": "Max Mustermann",
+    "telefonnummer": "0123456789",
+    "stadt": "Berlin"
+}
+
+# 2. Füge die E-Mail hinzu
+kontakt["email"] = "max@beispiel.de"
+
+# 3. Gib den Namen aus
+print(f"Der Name des Kontakes lautet: {kontakt["name"]}")
